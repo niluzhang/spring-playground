9383666dd826bcd1c553a5213d381983702f1b58
倪陆章 <1585288467@qq.com>
2018-08-21 14:20:15 +0800

    Update Main.java

--- src/com/huawei/nlz/springplayground/ch3/runtimevalueinject/Main.java
+++ src/com/huawei/nlz/springplayground/ch3/runtimevalueinject/Main.java
@@ -19,6 +19,8 @@ public class Main {
 
         EurekaInitializer ei = (EurekaInitializer) context2.getBean("eurekaInitializer");
         System.out.println(ei.getServiceName());
+        
+        context2.close();
     }
 
 }
