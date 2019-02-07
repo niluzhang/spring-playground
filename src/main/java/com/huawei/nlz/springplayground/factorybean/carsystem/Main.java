27e1ecd87de22ef637523208b97a631e23d32cf3
niluzhang <1585288467@qq.com>
2019-02-07 18:07:07 +0800

    modify log level of spring-framework.

--- src/main/java/com/huawei/nlz/springplayground/factorybean/carsystem/Main.java
+++ src/main/java/com/huawei/nlz/springplayground/factorybean/carsystem/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         /*
          * 调用getBean("car")时，Spring通过反射机制发现CarFactory实现了FactoryBean接口，这时Spring容器就调用
-         * CarFactory#getObject()方法返回。如果希望获取CarFactory实例，则在调用getBean(beanName)时在beanName前面
+         * CarFactory#getObject()方法返回。如果希望获取CarFactory实例，则需要在调用getBean(beanName)时在beanName前面
          * 加上&前缀，即getBean("&car")。
          */
         Car car = (Car) context.getBean("car");
