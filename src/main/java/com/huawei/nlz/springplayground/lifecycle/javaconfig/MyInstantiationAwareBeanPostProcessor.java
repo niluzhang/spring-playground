50a32dfc289fe0cb22ee45ce78cfde046293a000
nju-Nicko <1585288467@qq.com>
2019-08-25 21:51:33 +0800

    add bean lifecycle

--- src/main/java/com/huawei/nlz/springplayground/lifecycle/javaconfig/MyInstantiationAwareBeanPostProcessor.java
+++ src/main/java/com/huawei/nlz/springplayground/lifecycle/javaconfig/MyInstantiationAwareBeanPostProcessor.java
@@ -20,7 +20,14 @@ public class MyInstantiationAwareBeanPostProcessor extends
         }
     }
 
-    // 接口方法、实例化Bean之前调用
+    /**
+     * 接口方法、实例化Bean之前调用
+     *
+     * @param beanClass
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
     @Override
     public Object postProcessBeforeInstantiation(Class beanClass,
                                                  String beanName) throws BeansException {
@@ -30,7 +37,14 @@ public class MyInstantiationAwareBeanPostProcessor extends
         return null;
     }
 
-    // 接口方法、实例化Bean之后调用
+    /**
+     * 接口方法、实例化Bean之后调用
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName)
             throws BeansException {
@@ -40,7 +54,16 @@ public class MyInstantiationAwareBeanPostProcessor extends
         return bean;
     }
 
-    // 接口方法、设置某个属性时调用
+    /**
+     * 接口方法、设置某个属性时调用
+     *
+     * @param pvs
+     * @param pds
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
     @Override
     public PropertyValues postProcessPropertyValues(PropertyValues pvs,
                                                     PropertyDescriptor[] pds, Object bean, String beanName)
