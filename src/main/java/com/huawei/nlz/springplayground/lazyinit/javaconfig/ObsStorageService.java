ff62e429b709c15372ed055db183ae1e601fbb68
nju-Nicko <1585288467@qq.com>
2019-08-26 01:16:24 +0800

    add lazy init.

--- src/main/java/com/huawei/nlz/springplayground/lazyinit/javaconfig/ObsStorageService.java
+++ src/main/java/com/huawei/nlz/springplayground/lazyinit/javaconfig/ObsStorageService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@Lazy
+@Lazy // @Lazy注解可以与@Component或@Bean或@Configuration注解一起使用，标注对应的Bean或者对应配置里的Bean是否延迟初始化。
 public class ObsStorageService {
     public ObsStorageService() {
         if (log.isInfoEnabled()) {
