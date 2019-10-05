7b17b8d486a4f2e27130b7f74f81b10d224903cb
nju-Nicko <1585288467@qq.com>
2019-10-05 22:41:02 +0800

    添加spring事件注册发布模型的使用。

--- src/main/java/com/huawei/nlz/springplayground/eventmodel/Main.java
+++ src/main/java/com/huawei/nlz/springplayground/eventmodel/Main.java
@@ -9,6 +9,9 @@ public class Main {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(EventModelConfig.class);
         /*
          * ApplicationContext继承了ApplicationEventPublisher接口，可以发布事件。
+         *
+         * Spring读取配置类/配置文件之后，利用反射，将所有实现ApplicationListener的Bean找出来，
+         * 注册为容器的事件监听器。当接收到事件的时候，Spring会逐个调用事件监听器。
          */
         applicationContext.publishEvent(new SessionCreateEvent("app", UUID.randomUUID().toString()));
         applicationContext.close();
